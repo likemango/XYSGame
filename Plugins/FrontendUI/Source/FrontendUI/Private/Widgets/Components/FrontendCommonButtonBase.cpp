1e3d4e010b078cf05a514a3b95e50d802f915f6e
xiaoyao <330857910@qq.com>
2025-06-10 14:36:07 +0800

    FrontendCommonButtonBase

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/Components/FrontendCommonButtonBase.cpp
@@ -0,0 +1,20 @@
+﻿// XiaoYao copyright.
+
+
+#include "Widgets/Components/FrontendCommonButtonBase.h"
+#include "CommonTextBlock.h"
+
+void UFrontendCommonButtonBase::SetButtonText(FText InText)
+{
+	if (CommonTextBlock_ButtonText && !InText.IsEmpty())
+	{
+		CommonTextBlock_ButtonText->SetText(bUserUpperCaseForButtonText ? InText.ToUpper() : InText);
+	}
+}
+
+void UFrontendCommonButtonBase::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	SetButtonText(ButtonDisplayText);
+}
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/Components/FrontendCommonButtonBase.h
@@ -0,0 +1,42 @@
+﻿// XiaoYao copyright.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "CommonButtonBase.h"
+#include "FrontendCommonButtonBase.generated.h"
+
+class UCommonTextBlock;
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class FRONTENDUI_API UFrontendCommonButtonBase : public UCommonButtonBase
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetButtonText(FText InText);
+
+private:
+	//~begin UUserWidget interface
+	virtual void NativePreConstruct() override;
+	//~
+
+	/* Bound Widget */
+	UPROPERTY(meta = (BindWidgetOptional))
+	TObjectPtr<UCommonTextBlock> CommonTextBlock_ButtonText;
+	/* */
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Frontend Button", meta = (AllowPrivateAccess = "true"))
+	FText ButtonDisplayText;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Frontend Button", meta = (AllowPrivateAccess = "true"))
+	bool bUserUpperCaseForButtonText = false;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Frontend Button", meta = (AllowPrivateAccess = "true"))
+	FText ButtonDescriptionText;
+};
+
+
+
+
