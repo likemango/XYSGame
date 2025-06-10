db26cdf1a6d7573ce6f09f513752e06f5f459c97
xiaoyao <330857910@qq.com>
2025-06-10 17:51:28 +0800

    将TPTemplate导入InventoryPluginConent/并按照流程修改部分逻辑

---
+++ Plugins/Inventory/Source/Inventory/Private/Controller/Inv_PlayerController.cpp
@@ -0,0 +1,11 @@
+﻿// XiaoYao copyright.
+
+
+#include "Controller/Inv_PlayerController.h"
+
+#include "Inventory.h"
+
+AInv_PlayerController::AInv_PlayerController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	UE_LOG(LogInventory, Log, TEXT("Using Inv_PlayerController"));
+}
---
+++ Plugins/Inventory/Source/Inventory/Public/Controller/Inv_PlayerController.h
@@ -0,0 +1,19 @@
+﻿// XiaoYao copyright.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Inv_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API AInv_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AInv_PlayerController(const FObjectInitializer& ObjectInitializer);
+};
