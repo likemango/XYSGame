5f2bd1b57a16e4484e0aa2bce872c84be270ec85
xiaoyao <330857910@qq.com>
2025-06-13 13:20:30 +0800

    实现Portal NS

---
+++ Source/XYSGame/Private/Character/XYSPhysicalReactComponent.cpp
@@ -0,0 +1,14 @@
+﻿// XiaoYao copyright.
+
+
+#include "Character/XYSPhysicalReactComponent.h"
+
+
+UXYSPhysicalReactComponent::UXYSPhysicalReactComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+
+	
+
+}
