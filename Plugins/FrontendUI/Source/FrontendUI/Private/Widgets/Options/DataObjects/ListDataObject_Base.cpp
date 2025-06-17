0a58f58ee3e3d5030a80e0961c402af4d493edcb
xiaoyao <330857910@qq.com>
2025-06-17 17:37:17 +0800

    ChildListObejct_Base/Collection

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/Options/DataObjects/ListDataObject_Base.cpp
@@ -0,0 +1,13 @@
+﻿// XiaoYao copyright.
+
+
+#include "Widgets/Options/DataObjects/ListDataObject_Base.h"
+
+void UListDataObject_Base::InitDataObject()
+{
+	OnDataObjectInitialized();
+}
+
+void UListDataObject_Base::OnDataObjectInitialized()
+{
+}
