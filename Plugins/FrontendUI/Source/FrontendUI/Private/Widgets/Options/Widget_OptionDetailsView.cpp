ef4d8c7f78019f106847a603e4bb80ebab674955
xiaoyao <330857910@qq.com>
2025-06-23 11:40:16 +0800

    创建并添加DetailsView(使用ScrollBox可以很简单解决超过边界的问题)

---
+++ Plugins/FrontendUI/Source/FrontendUI/Private/Widgets/Options/Widget_OptionDetailsView.cpp
@@ -0,0 +1,4 @@
+﻿// XiaoYao copyright.
+
+
+#include "Widgets/Options/Widget_OptionDetailsView.h"
---
+++ Plugins/FrontendUI/Source/FrontendUI/Public/Widgets/Options/Widget_OptionDetailsView.h
@@ -0,0 +1,37 @@
+﻿// XiaoYao copyright.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Widget_OptionDetailsView.generated.h"
+
+class UCommonTextBlock;
+class UCommonLazyImage;
+class UCommonRichTextBlock;
+/**
+ * 
+ */
+UCLASS(Abstract, BlueprintType, meta = (DisableNativeTick))
+class FRONTENDUI_API UWidget_OptionDetailsView : public UUserWidget
+{
+	GENERATED_BODY()
+
+private:
+	//***** Bound Widgets *****//
+	UPROPERTY(meta = (BindWidget))
+	UCommonTextBlock* CommonTextBlock_Title;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonLazyImage* CommonLazyImage_DescriptionImage;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonRichTextBlock* CommonRichText_Description;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonRichTextBlock* CommonRichText_DynamicDetails;
+
+	UPROPERTY(meta = (BindWidget))
+	UCommonRichTextBlock* CommonRichText_DisabledReason;
+	//***** Bound Widgets *****//
+};
