3a611ea2e3628c8bd57b833b394c5ee2c95af046
xiaoyao <330857910@qq.com>
2025-06-30 18:44:05 +0800

    添加GA_Death和Ragdoll动画

--- Source/XYSGame/Private/System/XYSBlueprintFunctionLibrary.cpp
+++ Source/XYSGame/Private/System/XYSBlueprintFunctionLibrary.cpp
@@ -18,3 +18,112 @@ const UXYSInventoryItemFragment* UXYSInventoryFunctionLibrary::FindFragmentFromI
 }
 ///////////////////////////////////////////
 
+
+#include "Engine/AssetManagerTypes.h"
+#include "Engine/Engine.h"
+#include "Engine/World.h"
+#include "Engine/AssetManager.h"
+#include "XYSLogChannels.h"
+#include "Components/MeshComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(XYSBlueprintFunctionLibrary)
+
+TSoftObjectPtr<UObject> UXYSBlueprintFunctionLibrary::GetTypedSoftObjectReferenceFromPrimaryAssetId(FPrimaryAssetId PrimaryAssetId, TSubclassOf<UObject> ExpectedAssetType)
+{
+	if (UAssetManager* Manager = UAssetManager::GetIfInitialized())
+	{
+		FPrimaryAssetTypeInfo Info;
+		if (Manager->GetPrimaryAssetTypeInfo(PrimaryAssetId.PrimaryAssetType, Info) && !Info.bHasBlueprintClasses)
+		{
+			if (UClass* AssetClass = Info.AssetBaseClassLoaded)
+			{
+				if ((ExpectedAssetType == nullptr) || !AssetClass->IsChildOf(ExpectedAssetType))
+				{
+					return nullptr;
+				}
+			}
+			else
+			{
+				UE_LOG(LogXYSGame, Warning, TEXT("GetTypedSoftObjectReferenceFromPrimaryAssetId(%s, %s) - AssetBaseClassLoaded was unset so we couldn't validate it, returning null"),
+					*PrimaryAssetId.ToString(),
+					*GetPathNameSafe(*ExpectedAssetType));
+			}
+
+			return TSoftObjectPtr<UObject>(Manager->GetPrimaryAssetPath(PrimaryAssetId));
+		}
+	}
+	return nullptr;
+}
+
+// FPrimaryAssetId UXYSBlueprintFunctionLibrary::GetPrimaryAssetIdFromUserFacingExperienceName(const FString& AdvertisedExperienceID)
+// {
+// 	const FPrimaryAssetType Type(UXYSUserFacingExperienceDefinition::StaticClass()->GetFName());
+// 	return FPrimaryAssetId(Type, FName(*AdvertisedExperienceID));
+// }
+
+void UXYSBlueprintFunctionLibrary::PlayNextGame(const UObject* WorldContextObject)
+{
+	UWorld* World = GEngine->GetWorldFromContextObject(WorldContextObject, EGetWorldErrorMode::LogAndReturnNull);
+	if (World == nullptr)
+	{
+		return;
+	}
+
+	const FWorldContext& WorldContext = GEngine->GetWorldContextFromWorldChecked(World);
+	FURL LastURL = WorldContext.LastURL;
+
+#if WITH_EDITOR
+	// To transition during PIE we need to strip the PIE prefix from maps.
+	LastURL.Map = UWorld::StripPIEPrefixFromPackageName(LastURL.Map, WorldContext.World()->StreamingLevelsPrefix);
+#endif
+
+	// Add seamless travel option as we want to keep clients connected. This will fall back to hard travel if seamless is disabled
+	LastURL.AddOption(TEXT("SeamlessTravel"));
+
+	FString URL = LastURL.ToString();
+	// If we don't remove the host/port info the server travel will fail.
+	URL.RemoveFromStart(LastURL.GetHostPortString());
+	
+	const bool bAbsolute = false; // we want to use TRAVEL_Relative
+	const bool bShouldSkipGameNotify = false;
+	World->ServerTravel(URL, bAbsolute, bShouldSkipGameNotify);
+}
+
+void UXYSBlueprintFunctionLibrary::SetScalarParameterValueOnAllMeshComponents(AActor* TargetActor, const FName ParameterName, const float ParameterValue, bool bIncludeChildActors)
+{
+	if (TargetActor != nullptr)
+	{
+		TargetActor->ForEachComponent<UMeshComponent>(bIncludeChildActors, [=](UMeshComponent* InComponent)
+		{
+			InComponent->SetScalarParameterValueOnMaterials(ParameterName, ParameterValue);
+		});
+	}
+}
+
+void UXYSBlueprintFunctionLibrary::SetVectorParameterValueOnAllMeshComponents(AActor* TargetActor, const FName ParameterName, const FVector ParameterValue, bool bIncludeChildActors)
+{
+	if (TargetActor != nullptr)
+	{
+		TargetActor->ForEachComponent<UMeshComponent>(bIncludeChildActors, [=](UMeshComponent* InComponent)
+		{
+			InComponent->SetVectorParameterValueOnMaterials(ParameterName, ParameterValue);
+		});
+	}
+}
+
+void UXYSBlueprintFunctionLibrary::SetColorParameterValueOnAllMeshComponents(AActor* TargetActor, const FName ParameterName, const FLinearColor ParameterValue, bool bIncludeChildActors)
+{
+	SetVectorParameterValueOnAllMeshComponents(TargetActor, ParameterName, FVector(ParameterValue), bIncludeChildActors);
+}
+
+TArray<UActorComponent*> UXYSBlueprintFunctionLibrary::FindComponentsByClass(AActor* TargetActor, TSubclassOf<UActorComponent> ComponentClass, bool bIncludeChildActors)
+{
+	TArray<UActorComponent*> Components;
+	if (TargetActor != nullptr)
+	{
+		TargetActor->GetComponents(ComponentClass, /*out*/ Components, bIncludeChildActors);
+
+	}
+	return MoveTemp(Components);
+}
+
--- Source/XYSGame/Public/System/XYSBlueprintFunctionLibrary.h
+++ Source/XYSGame/Public/System/XYSBlueprintFunctionLibrary.h
@@ -18,6 +18,32 @@ class XYSGAME_API UXYSBlueprintFunctionLibrary : public UBlueprintFunctionLibrar
 {
 	GENERATED_BODY()
 
+public:
+			/** Returns the soft object reference associated with a Primary Asset Id, this works even if the asset is not loaded */
+	UFUNCTION(BlueprintPure, Category = "AssetManager", meta=(DeterminesOutputType=ExpectedAssetType))
+	static TSoftObjectPtr<UObject> GetTypedSoftObjectReferenceFromPrimaryAssetId(FPrimaryAssetId PrimaryAssetId, TSubclassOf<UObject> ExpectedAssetType);
+
+	// UFUNCTION(BlueprintCallable)
+	// static FPrimaryAssetId GetPrimaryAssetIdFromUserFacingExperienceName(const FString& AdvertisedExperienceID);
+
+	UFUNCTION(BlueprintCallable, BlueprintAuthorityOnly, Category="Lyra", meta = (WorldContext = "WorldContextObject"))
+	static void PlayNextGame(const UObject* WorldContextObject);
+
+	// Sets ParameterName to ParameterValue on all sections of all mesh components found on the TargetActor
+	UFUNCTION(BlueprintCallable, Category = "Rendering|Material", meta=(DefaultToSelf="TargetActor"))
+	static void SetScalarParameterValueOnAllMeshComponents(AActor* TargetActor, const FName ParameterName, const float ParameterValue, bool bIncludeChildActors = true);
+
+	// Sets ParameterName to ParameterValue on all sections of all mesh components found on the TargetActor
+	UFUNCTION(BlueprintCallable, Category = "Rendering|Material", meta=(DefaultToSelf="TargetActor"))
+	static void SetVectorParameterValueOnAllMeshComponents(AActor* TargetActor, const FName ParameterName, const FVector ParameterValue, bool bIncludeChildActors = true);
+
+	// Sets ParameterName to ParameterValue on all sections of all mesh components found on the TargetActor
+	UFUNCTION(BlueprintCallable, Category = "Rendering|Material", meta=(DefaultToSelf="TargetActor"))
+	static void SetColorParameterValueOnAllMeshComponents(AActor* TargetActor, const FName ParameterName, const FLinearColor ParameterValue, bool bIncludeChildActors = true);
+
+	// Gets all the components that inherit from the given class
+	UFUNCTION(BlueprintCallable, Category = "Actor", meta=(DefaultToSelf="TargetActor", ComponentClass="/Script/Engine.ActorComponent", DeterminesOutputType="ComponentClass"))
+	static TArray<UActorComponent*> FindComponentsByClass(AActor* TargetActor, TSubclassOf<UActorComponent> ComponentClass, bool bIncludeChildActors = true);
 };
 
 UCLASS()
